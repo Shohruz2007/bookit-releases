74943dc5cfdb7af1beeca25495e06244b4819d77
Shohruz2007 <Shohruz2007@users.noreply.github.com>
2026-07-29 19:41:36 +0000

    deploy: dnaz1m/bookit-mobile@46028660e9e90d35a3d2dc66244a954df0426c74

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,14 +4,14 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "f0673ff2ac73151ba513beaac560527e",
+"flutter_bootstrap.js": "3bae494cc0260c792e0a00096d92e31a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "index.html": "fc854e12133c67582053bf6e01f85635",
 "/": "fc854e12133c67582053bf6e01f85635",
-"main.dart.js": "509fe8ca57981d722fdae13f7005fa1a",
+"main.dart.js": "b446c5af355c8622a9d882f855bab76f",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "canvaskit/skwasm.js": "ea559890a088fe28b4ddf70e17e60052",
