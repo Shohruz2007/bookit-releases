9f2eb7558f7053aa5c97d3d710dd8273b4b5616a
Shohruz2007 <Shohruz2007@users.noreply.github.com>
2026-08-06 17:41:33 +0000

    deploy: dnaz1m/bookit-mobile@266025cf5a1ae1c5e44d9a40892d0b4add825c18

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "852953965"
+    serviceWorkerVersion: "451339808"
   }
 });
