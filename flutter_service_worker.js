e03a654860b7e291b8197b26b1d1b3be4c67ff4b
Shohruz2007 <Shohruz2007@users.noreply.github.com>
2026-08-21 20:15:40 +0000

    deploy: dnaz1m/bookit-mobile@536caa43ce325cc2ce030cf7d9fbb5e5dfa4e22f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -78,8 +78,8 @@ const RESOURCES = {"version.json": "9c1892e7e4290fd1c6e5226fa37e9bd3",
 "assets/assets/icons/eye_off.svg": "cef8f080373ec72a305b873d644a4e7e",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/packages/flutter_map/lib/assets/flutter_map_logo.png": "208d63cc917af9713fc9572bd5c09362",
-"main.dart.js": "d1510bfd97a69317615459d552c3a02e",
-"flutter_bootstrap.js": "06cc810694196f31bd10401aa572c457",
+"main.dart.js": "1e5cc0cdf306ef614ff238063c16ca62",
+"flutter_bootstrap.js": "3ed67689fe74bff66a2bfa25f4fe01cb",
 "manifest.json": "a557db73668aae832682ecbd61e48423"};
 // The application shell files that are downloaded before a service worker can
 // start.
